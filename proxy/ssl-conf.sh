d67e787c464c6f47ba0e4b943a5641ad8ba456f0
Erdal A <erdalaltin@gmail.com>
2022-02-27 12:44:00 +0300

    Update ssl-conf.sh

--- proxy/ssl-conf.sh
+++ proxy/ssl-conf.sh
@@ -12,10 +12,10 @@ fi
 
 use_lets_encrypt_certificates() {
 	echo "Switching Nginx to use Let's Encrypt certificate for $1"	
-	sed -i '/location.*acme-challenge/,/}/ s/^[^#]/#/' $PROXY_PREFIX/conf.d/default.conf
-	sed -i '/#location.\/./,/#}/ s/#//' $PROXY_PREFIX/conf.d/default.conf
-	sed -i 's/#listen/listen/g' $PROXY_PREFIX/conf.d/default.conf
-	sed -i 's/#ssl_/ssl_/g' $PROXY_PREFIX/conf.d/default.conf	
+	sed -i.bak '/location.*acme-challenge/,/}/ s/^[^#]/#/ /#location.\/./,/#}/ s/#// s/#listen/listen/g s/#ssl_/ssl_/g' $PROXY_PREFIX/conf.d/default.conf
+	#sed -i '/#location.\/./,/#}/ s/#//' $PROXY_PREFIX/conf.d/default.conf
+	#sed -i 's/#listen/listen/g' $PROXY_PREFIX/conf.d/default.conf
+	#sed -i 's/#ssl_/ssl_/g' $PROXY_PREFIX/conf.d/default.conf	
 }
 
 reload_nginx() {
