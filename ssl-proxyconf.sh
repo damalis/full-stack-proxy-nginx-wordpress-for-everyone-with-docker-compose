cd4ccf98aad33ef1c39aba7de398aaaff1321174
Erdal A <erdalaltin@gmail.com>
2022-03-02 19:04:48 +0300

    Update ssl-proxyconf.sh

--- ssl-proxyconf.sh
+++ ssl-proxyconf.sh
@@ -12,30 +12,31 @@ fi
 
 use_lets_encrypt_certificates() {
 	echo "switching nginx to use Let's Encrypt certificate for $1"	
-	sed -i '/location.*acme-challenge/,/}/ s/^[^#]/#/; /#location.\/./,/#}/ s/#//; s/#listen/listen/g; s/#ssl_/ssl_/g' $3/conf.d/default.conf > $3/conf.d/default.conf.bak && cp $3/conf.d/default.conf.bak $3/conf.d/default.conf	
+	sed '/location.*acme-challenge/,/}/ s/^[^#]/#/; /#location.\/./,/#}/ s/#//; s/#listen/listen/g; s/#ssl_/ssl_/g' $3/conf.d/default.conf > $3/conf.d/default.conf.bak
 }
 
 reload_nginx() {
+	cp $1/conf.d/default.conf.bak $1/conf.d/default.conf	
 	echo "Reloading Nginx configuration"
 	nginx -s reload
 }
 
 wait_for_lets_encrypt() {
-	if [ -n "$(find $2/live -name '$1-*' | head -1)" ]; then 
+	if [ -n "$(find $2/live -name '$1*' | head -1)" ]; then 
 		break 
 	else
-		until [ -n "$(find $2/live -name '$1-*' | head -1)" ]; do
+		until [ -n "$(find $2/live -name '$1*' 2>/dev/null | head -1)" ]; do
 			echo "waiting for Let's Encrypt certificates for $1"
 			sleep 5s & wait ${!}
-			if [ -n "$(find $2/live -name '$1-*' | head -1)" ]; then break; fi
+			if [ -n "$(find $2/live -name '$1*' | head -1)" ]; then break; fi
 		done
 	fi;	
 	use_lets_encrypt_certificates "$1" "$2" "$3"
-	reload_nginx
+	reload_nginx "$3"
 }
 
 for domain in $1; do
-	if [ -n "$(find $2/live -name '$1-*' | head -1)" ]; then
+	if [ -n "$(find $2/live -name '$domain*' | head -1)" ]; then
 		wait_for_lets_encrypt "$domain" "$2" "$3" &
 	else
 		use_lets_encrypt_certificates "$domain" "$2" "$3"
