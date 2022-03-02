a763c3c0af5515072a107bc1364918d27251d4fe
Erdal A <erdalaltin@gmail.com>
2022-03-02 18:34:18 +0300

    Update ssl-proxyconf.sh

--- ssl-proxyconf.sh
+++ ssl-proxyconf.sh
@@ -1,50 +1,45 @@
 #!/bin/sh
 set -e
 
-DOMAIN=$1
-
 if [ -z $1 ]; then
 	echo "DOMAIN environment variable is not set"
 	exit 1;
 fi
 
-if [ ! -f ./certbot/ssl-dhparam.pem ]; then
+if [ ! -f $2/ssl-dhparam.pem ]; then
 	openssl dhparam -out $2/ssl-dhparam.pem 2048
 fi
 
 use_lets_encrypt_certificates() {
 	echo "switching nginx to use Let's Encrypt certificate for $1"	
-	sed -i '/location.*acme-challenge/,/}/ s/^[^#]/#/; /#location.\/./,/#}/ s/#//; s/#listen/listen/g; s/#ssl_/ssl_/g' ./proxy/conf.d/proxy.conf	
+	sed -i '/location.*acme-challenge/,/}/ s/^[^#]/#/; /#location.\/./,/#}/ s/#//; s/#listen/listen/g; s/#ssl_/ssl_/g' $3/conf.d/default.conf > $3/conf.d/default.conf.bak && cp $3/conf.d/default.conf.bak $3/conf.d/default.conf	
 }
 
 reload_nginx() {
 	echo "Reloading Nginx configuration"
-	docker-compose stop proxy
-	docker-compose start proxy
+	nginx -s reload
 }
 
 wait_for_lets_encrypt() {
-	if sudo [ -d "./certbot/live/$1" ]; then 
+	if [ -n "$(find $2/live -name '$1-*' | head -1)" ]; then 
 		break 
 	else
-		until sudo ls ./certbot/live/$1 2>/dev/null; do
+		until [ -n "$(find $2/live -name '$1-*' | head -1)" ]; do
 			echo "waiting for Let's Encrypt certificates for $1"
 			sleep 5s & wait ${!}
-			if sudo [ -d "./certbot/live/$1" ]; then break; fi
+			if [ -n "$(find $2/live -name '$1-*' | head -1)" ]; then break; fi
 		done
 	fi;	
 	use_lets_encrypt_certificates "$1" "$2" "$3"
 	reload_nginx
-	exit 0
 }
 
-for domain in $DOMAIN; do
-	if sudo [ ! -d "$2/live/$domain" ]; then
-		wait_for_lets_encrypt "$domain" &
+for domain in $1; do
+	if [ -n "$(find $2/live -name '$1-*' | head -1)" ]; then
+		wait_for_lets_encrypt "$domain" "$2" "$3" &
 	else
-		use_lets_encrypt_certificates "$domain"
+		use_lets_encrypt_certificates "$domain" "$2" "$3"
 	fi
 done
 
-#docker-compose stop proxy
-#docker-compose start proxy
+exec nginx -g "daemon off;"
