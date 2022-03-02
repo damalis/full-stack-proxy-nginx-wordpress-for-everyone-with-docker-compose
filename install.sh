21718498b70deca6c7c182fed0eba8a3471a355e
Erdal A <erdalaltin@gmail.com>
2022-03-02 11:56:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -236,13 +236,13 @@ if [ -x "$(command -v docker)" ] && [ -x "$(command -v docker-compose)" ]; then
 		if [ $? -ne 0 ]; then
 			echo "Error! could not installed portainer" >&2
 			exit 1
-		else			
+		else
+			./ssl-proxyconf.sh "$domain_name" &
 			until sudo ls ./certbot/live/$domain_name 2>/dev/null; do
-				echo "waiting for Let's Encrypt certificates for $1"
-				sleep 5s & wait ${!}
+				echo "waiting for Let's Encrypt certificates for $domain_name"
+				sleep 6s & wait ${!}
 				if sudo [ -d "./certbot/live/$domain_name" ]; then break; fi
-			done
-			
+			done			
 			echo ""
 			echo "completed setup"
 			echo ""
