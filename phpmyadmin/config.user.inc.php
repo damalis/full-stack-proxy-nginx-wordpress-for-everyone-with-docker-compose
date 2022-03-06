bf1f650d693002d20138d384d161d03489320111
Erdal A <erdalaltin@gmail.com>
2022-03-06 12:38:08 +0300

    Update config.user.inc.php

--- phpmyadmin/config.user.inc.php
+++ phpmyadmin/config.user.inc.php
@@ -1,5 +1,8 @@
 <?php
 
+/* We now have to tell phpMyAdmin that SSL must be used whenever a connection is made. */
+$cfg['ForceSSL'] = true;
+
 /* Uploads setup */
 $cfg['UploadDir'] = $_ENV['PMA_APACHE2_PREFIX'] . '/../upload';
 $cfg['SaveDir'] = $_ENV['PMA_APACHE2_PREFIX'] . '/../save';
