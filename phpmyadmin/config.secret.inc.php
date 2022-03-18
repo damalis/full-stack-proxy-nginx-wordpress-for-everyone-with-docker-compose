877143cda75fd65eaa582e4edcd81feb2138f61d
Erdal A <erdalaltin@gmail.com>
2022-03-18 11:51:45 +0300

    Create config.secret.inc.php

---
+++ phpmyadmin/config.secret.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This is needed for cookie based authentication to encrypt password in
+ * cookie. Needs to be 32 chars long.
+ */
+ 
+$cfg['blowfish_secret'] = 'A0FJ}K;NvySbk9=aJEO:pcW4S3o9:8X0'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
