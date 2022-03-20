1d1ed8fb7e6c5cdce145a9c5a4b4ba7bc82b5900
Erdal A <erdalaltin@gmail.com>
2022-03-20 12:51:48 +0300

    Update config.secret.inc.php

--- phpmyadmin/config.secret.inc.php
+++ phpmyadmin/config.secret.inc.php
@@ -5,4 +5,4 @@
  * cookie. Needs to be 32 chars long.
  */
  
-$cfg['blowfish_secret'] = 'A0FJ}K;NvySbk9=aJEO:pcW4S3o9:8X0'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
