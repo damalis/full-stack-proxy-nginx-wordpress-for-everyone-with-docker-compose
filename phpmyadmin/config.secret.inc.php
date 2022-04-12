8f77d73a47e0d7af0f9f2b2821fa8fa9782ac7bd
Erdal ALTIN <erdalaltin@gmail.com>
2022-04-12 18:45:35 +0300

    Delete config.secret.inc.php

--- phpmyadmin/config.secret.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * This is needed for cookie based authentication to encrypt password in
- * cookie. Needs to be 32 chars long.
- */
- 
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
