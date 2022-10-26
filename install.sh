80477f7d1eaee91b6df8d8ca2a74866ddfccdce9
Erdal ALTIN <erdalaltin@gmail.com>
2022-10-26 11:01:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -263,8 +263,8 @@ if [ -x "$(command -v docker)" ] && [ -x "$(command -v docker-compose)" ]; then
 				if [ ! -z `docker ps -q -f "status=running" --no-trunc | grep $(docker-compose ps -q proxy)` ]; then break; fi
 			done			
 			echo ""
-			echo "Reloading Proxy Nginx ssl configuration"
-			docker exec proxy nginx -s reload > /dev/null 2>&1
+			echo "Reloading proxy ssl configuration"
+			docker container restart proxy > /dev/null 2>&1
 			echo "Ok."
 			echo ""
 			echo "completed setup"
