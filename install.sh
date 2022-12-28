9d40b1c76b229b90480ac43c825502a0aa2d313e
Erdal ALTIN <erdalaltin@gmail.com>
2022-12-28 16:39:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,6 @@ sudo apt-get install ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(grep -Po 'UBUNTU_CODENAME=\K[^;]*' /etc/os-release) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo chmod 666 /var/run/docker.sock
 sudo apt-get update
 
 Installed=`sudo apt-cache policy docker-ce | sed -n '2p' | cut -c 14-`
