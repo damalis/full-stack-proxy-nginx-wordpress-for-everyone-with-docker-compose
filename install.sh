9182b0ba204f61e1b79c71d97c10ca1dff72c484
Erdal ALTIN <erdalaltin@gmail.com>
2026-03-30 16:40:19 +0300

    Improve docker-compose configs and installer robustness

--- install.sh
+++ install.sh
@@ -276,10 +276,10 @@ fi
 oc_output=$(cat /proc/sys/vm/overcommit_memory 2>&1)
 if [ $oc_output != 1 ]
 then
-        sudo sysctl -w vm.overcommit_memory=1
-        echo "vm.overcommit_memory = 1" | sudo tee -a /etc/sysctl.conff > /dev/null
-        # Apply sysctl params without reboot
-        sudo sysctl --system > /dev/null 2>&1
+	sudo sysctl -w vm.overcommit_memory=1
+	echo "vm.overcommit_memory = 1" | sudo tee -a /etc/sysctl.conf > /dev/null
+	# Apply sysctl params without reboot
+	sudo sysctl --system > /dev/null 2>&1
 fi
 
 if ps -p 1 -o comm= | grep -q systemd
@@ -366,21 +366,13 @@ sleep 2
 
 # set the host
 which_h=""
-items=("localhost" "remotehost")
-PS3="which computer command line are you on? Select the host: "
-select h in "${items[@]}"
-do
-	case $REPLY in
-		1)
-			which_h=$h
-			break;;
-		2)
-			which_h=$h
-			break;;
-		*)
-			echo "Invalid choice $REPLY";;
-	esac
-done
+if [ -n "${SSH_CLIENT}" ] || [ -n "${SSH_TTY}" ] || [ -n "${SSH_CONNECTION}" ]; then
+	echo "Running REMOTELY via SSH (on another host / remote OS)"
+	which_h="remotehost"
+else
+	echo "Running LOCALLY on localhost (the same machine)"
+	which_h="localhost"
+fi
 echo "Ok."
 
 # set your domain name
